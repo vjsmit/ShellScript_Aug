3312f40a3010528ccf8e4266178bb3d5c132756d
vjsmit <vj.smit97@gmail.com>
2021-08-29 11:02:54 -0400

    no message

---
+++ 4-Inputs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+read -p 'Enter your name: ' name
+read -p 'Enter your course: ' course
+read -s -p 'Enter your password: ' password
+
+
+echo -e "\n\nHello $name, Welcome to $course course, your pwd is $password"
