dca86581e9966fd6bb0f6d79cddba3c4986467d6
vjsmit <vj.smit97@gmail.com>
2021-09-02 08:36:37 -0400

    no message

--- 8-Function.sh
+++ 8-Function.sh
@@ -17,8 +17,16 @@ sampley 18 10
 
 samplez() {
     echo Hello
-    return
+    return 50
 }
 
 samplez
 echo "Status of the function = $?"
+
+sample_var() {
+    af=100
+    echo "Value of am is $am"
+}
+
+am=200
+echo "Value of af is $af"
