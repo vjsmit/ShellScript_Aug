59ea940ac20634a174635a082ccfc4e293477aee
vjsmit <vj.smit97@gmail.com>
2021-09-02 09:22:32 -0400

    no message

---
+++ 10-if.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+R="\e[33m"
+N="\e[0m"
+
+action=$1
+
+if ["$action"="start"]; then
+    echo "Starting the service"
+
+elif["$action"="stop"]; then
+    echo "Stoping the service"
+else
+    echo "Usage:: $0 start|stop"
+    echo "${R}start${N} ->starting the service"
+    echo "${R}stop${N} -> stoping the service"
