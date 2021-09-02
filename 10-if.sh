109ce4c5b9a57449ebf237236d052a50b0a7c7e1
vjsmit <vj.smit97@gmail.com>
2021-09-02 09:57:38 -0400

    no message

--- 10-if.sh
+++ 10-if.sh
@@ -9,10 +9,10 @@ if [ "$action" = "start" ]; then
     echo "Starting the service"
 elif [ "$action"="stop" ]; then
     echo "Stoping the service"
-else
+else 
     echo "Usage:: $0 start|stop"
-    echo -e "${R}start${N} ->starting the service"
-    echo -e "${R}stop${N} -> stoping the service"
+    echo -e "\t ${R}start${N} -> Start the service"
+    echo -e "\t ${R}stop${N}  -> Stop the service"
     exit 1
 fi
 
@@ -26,4 +26,10 @@ fi
 #     echo -e "\t ${R}stop${N}  -> Stop the service"
 #     exit 1
 # fi
+
+# else
+#     echo "Usage:: $0 start|stop"
+#     echo -e "${R}start${N} ->starting the service"
+#     echo -e "${R}stop${N} -> stoping the service"
+#     exit 1
 
