5dd0e29bbc4160b159361c5ace8334372d0499a2
vjsmit <vj.smit97@gmail.com>
2021-09-03 08:42:26 -0400

    no message

--- rough.sh
+++ rough.sh
@@ -6,7 +6,7 @@ rm -f /tmp/stack.log
 APPUSER=student
 APPHOME=/home/$APPUSER
 ## Fetch the latest available version 
-TOMCAT_VERSION=$(curl -s 'https://archive.apache.org/dist/tomcat/tomcat-9/?C=M;O=A' | grep 9.0 | tail -1 | awk '{print $5}' | a   -F '"' '{print $2}' | sed -e 's/v//' -e 's/\///)
+TOMCAT_VERSION=$(curl -s 'https://archive.apache.org/dist/tomcat/tomcat-9/?C=M;O=A' | grep 9.0 | tail -1 | awk '{print $5}' | a   -F '"' '{print $2}' | sed -e 's/v//' -e 's/\///')
 TOMCAT_URL="https://archive.apache.org/dist/tomcat/tomcat-8/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz"
 TOMCAT_DIR=$APPHOME/apache-tomcat-$TOMCAT_VERSION
 
@@ -115,4 +115,4 @@ Stat $?
 
 Print "Fix Permissions\t\t"
 chown $APPUSER:$APPUSER $TOMCAT_DIR -R &>>$LOG 
-Stat $? 
+Stat $?
