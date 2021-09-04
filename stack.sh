7a7de34b189a713dc5948bc74eb06ba036dafaaa
vjsmit <vj.smit97@gmail.com>
2021-09-04 04:24:06 -0400

    no message

--- stack.sh
+++ stack.sh
@@ -105,7 +105,9 @@ chown $App_user:$App_user $Tomcat_DIR -R &>>$LOG
 Stat $?
 
 Print "Setup DB parameters"
-sed -i -e '$ i <Resource name="jdbc/TestDB" auth="Container" type="javax.sql.DataSource" maxTotal="100" maxIdle="30" maxWaitMillis="10000" username="USERNAME" password="PASSWORD" driverClassName="com.mysql.jdbc.Driver" url="jdbc:mysql://RDS-DB-ENDPOINT:3306/DATABASE"/>' $Tomcat_DIR/conf/context.xml
+sed -i -e '$ i <Resource name="jdbc/TestDB" auth="Container" type="javax.sql.DataSource" maxTotal="100" maxIdle="30" 
+                maxWaitMillis="10000" username="USERNAME" password="PASSWORD" driverClassName="com.mysql.jdbc.Driver" 
+                url="jdbc:mysql://RDS-DB-ENDPOINT:3306/DATABASE"/>' $Tomcat_DIR/conf/context.xml
 Stat $?
 
 
